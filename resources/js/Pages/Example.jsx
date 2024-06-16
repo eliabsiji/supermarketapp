233ba64aef0e75161204d59a16b890a0085e9168
eliab <eliabsiji@gmail.com>
2024-06-16 15:01:11 +0100

    first commit

---
+++ resources/js/Pages/Example.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Example extends Component {
+  render() {
+    return (
+      <div>
+        welcome
+      </div>
+    )
+  }
+}
+
+export default Example
