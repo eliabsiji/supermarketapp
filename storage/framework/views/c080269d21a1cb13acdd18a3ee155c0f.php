d7d37986a3b5a26a6137a93d1951fafde87a1817
Eliab <Eliabsiji@gmail.com>
2024-06-16 23:43:05 +0100

    first commit on main

---
+++ storage/framework/views/c080269d21a1cb13acdd18a3ee155c0f.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?> 
+        <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.jsx']); ?>
+        <!-- In this article, we are going to use JSX syntax for React components -->
+        <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+    </head>
+    <body>
+        <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\supermarketapp\resources\views/app.blade.php ENDPATH**/ ?>
