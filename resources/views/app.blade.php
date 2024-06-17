911ed2f8f6fb59d94f861361e7258af939f6a03b
SilasIlem <danryan0246@gmail.com>
2024-06-17 15:29:54 +0100

    anon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,60 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<html lang="en" >
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8"/>
+        <meta name="description" content="
+            The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, 
+            Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. 
+            Grab your copy now and get life-time updates for free.
+        "/>
+        <meta name="keywords" content="
+            metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, 
+            Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, 
+            free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, 
+            bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon
+        "/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>      
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="
+            Metronic - Bootstrap Admin Template, HTML, VueJS, React, Angular. Laravel, Asp.Net Core, Ruby on Rails, 
+            Spring Boot, Blazor, Django, Express.js, Node.js, Flask Admin Dashboard Theme & Template
+        " />
+        <meta property="og:url" content="https://keenthemes.com/metronic"/>
+        <meta property="og:site_name" content="Keenthemes | Metronic" />
+        <link rel="canonical" href="https://preview.keenthemes.com/metronic8"/>
+        <link rel="shortcut icon" href="../../../assets/media/logos/favicon.ico"/>
+        <!--begin::Fonts(mandatory for all pages)-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700"/>        <!--end::Fonts-->
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+        <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css"/>
+        <!--end::Global Stylesheets Bundle-->
 
+        <!--Begin::Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '../../../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5FS8GGP');</script>
+        <!--End::Google Tag Manager -->
+                
+        <script>
+            // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
+            if (window.top != window.self) {
+                window.top.location.replace(window.self.location.href);
+            }
+        </script>
+    </head>
+    <!--end::Head-->
+
+    <!--begin::Body-->
+    <body  id="kt_body"  class="app-blank" >
+            <!--end::Body--> 
         <!-- Scripts -->
         @routes
         @viteReactRefresh
@@ -18,5 +63,38 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
+        <!--begin::Theme mode setup on page load-->
+        <script>
+            var defaultThemeMode = "light";
+            var themeMode;
+
+            if ( document.documentElement ) {
+                if ( document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                    themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+                } else {
+                    if ( localStorage.getItem("data-bs-theme") !== null ) {
+                        themeMode = localStorage.getItem("data-bs-theme");
+                    } else {
+                        themeMode = defaultThemeMode;
+                    }			
+                }
+
+                if (themeMode === "system") {
+                    themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+                }
+
+                document.documentElement.setAttribute("data-bs-theme", themeMode);
+            }            
+        </script>
+        <!--end::Theme mode setup on page load-->            
+                            <!--Begin::Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!--End::Google Tag Manager (noscript) -->
+       
+    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
+        <script src="{{asset('assets/plugins/global/plugins.bundle.js')}}></script>
+        <script src="{{asset('../../../assets/js/scripts.bundle.js')}}"></script>
+    <!--end::Global Javascript Bundle-->
+
     </body>
 </html>
